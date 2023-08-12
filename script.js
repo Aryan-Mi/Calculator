9da523126b34f5b79a4066a0e9b8e482b6464b21
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-08-12 17:13:11 +0200

    Added num, dec & backspace handlers
    Also adjusted overflow of the design for large numbers

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
-let calculated = "";
+let calculated = "420";
 let input = "0";
 
 const buttons = document.querySelectorAll(".button");
 const inputSpan = document.querySelector(".input > span");
 const calcSpan = document.querySelector(".calculation > span");
 
-console.log(buttons);
-
 buttons.forEach((button) =>
 	button.addEventListener("click", () => {
-		console.log("Pressed", button.textContent);
-		calculated = input.trim();
-		calcSpan.textContent = calculated;
-		input = button.textContent;
-		inputSpan.textContent = input;
+		handleButton(button.attributes["data-button"].value);
+		updateInputSpan();
+
+		// calculated = input.trim();
+		// calcSpan.textContent = calculated;
+		// input = button.textContent;
+		// inputSpan.textContent = input;
 	})
 );
 
-function handleButton(button) {
-	switch (button) {
-		case 1:
+function handleButton(buttonValue) {
+	switch (buttonValue) {
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+			handleNumbers(buttonValue);
+			break;
+		case ".":
+			handleDecimal();
+			break;
+		case "backspace":
+			handleBackSpace();
 			break;
 		default:
 			break;
 	}
+}
+
+// ! BUTTON HANDLERS
+function handleNumbers(number) {
+	if (Number(input) === 0 || input === "" || isNaN(input)) {
+		input = number;
+	} else if (input.length < 16 && input < Number.MAX_SAFE_INTEGER)
+		input += number;
+	13;
+}
+
+function handleDecimal() {
+	console.log(input);
+	if (input === "" || isNaN(input)) {
+		console.log("inside", input, inputSpan.textContent);
+		input = "0.";
+	} else {
+		input += !input.includes(".") ? "." : "";
+	}
+}
+
+function handleBackSpace() {
+	input = input.slice(0, -1);
+}
+
+function handleSignChange() {
+	input = input.charAt(0) === "-" ? input.slice(1) : (input = `-${input}`);
 }
 
 function evalInput() {}
@@ -46,4 +89,18 @@ function module(a, b) {
 }
 function plusminus(a) {
 	return a * -1;
+}
+
+// Auxilary Functions
+
+function updateInputSpan() {
+	const len = input.length;
+
+	if (len < 8) {
+		inputSpan.textContent = input;
+		return;
+	}
+
+	// Trim input from left if longer than max-width = 8ch
+	inputSpan.textContent = `${input}`.substring(len - 8);
 }
