b87375c7a841e8494d3d2ec213b19206d7997730
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-08-13 14:08:33 +0200

    Added all operator functionality, eval and clear all

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const CALC_OPERATORS = ["+", "-", "*", "/", "%"];
+// const CALC_OPERATORS = ["+", "-", "*", "/", "%"];
+const CALC_FUNCTIONS = {
+	"+": (a, b) => `${Number(a) + Number(b)}`,
+	"-": (a, b) => `${Number(a) - Number(b)}`,
+	"×": (a, b) => `${Number(a) * Number(b)}`,
+	"/": (a, b) => `${Number(a) / Number(b)}`,
+	"%": (a, b) => `${Number(a) % Number(b)}`,
+};
+
 // const DECIMALS = ["1", "2", "3", "4", "5", "6", "7", "9", "0", "."];
 
 let calculation = "";
 let input = "0";
-// const includesOperator = CALC_OPERATORS.includes(calculation.slice(-1));
 let operatorMode = false;
 
 const buttons = document.querySelectorAll(".button");
@@ -13,12 +20,8 @@ const calcSpan = document.querySelector(".calculation > span");
 buttons.forEach((button) =>
 	button.addEventListener("click", () => {
 		handleButton(button.attributes["data-button"].value);
-		updateInputSpan();
-		updateCalcSpan();
-		// calculated = input.trim();
-		// calcSpan.textContent = calculated;
-		// input = button.textContent;
-		// inputSpan.textContent = input;
+		updateSpan(input, inputSpan, 8);
+		updateSpan(calculation, calcSpan, 70);
 	})
 );
 
@@ -46,7 +49,17 @@ function handleButton(buttonValue) {
 			handleSignChange();
 			break;
 		case "+":
+		case "-":
+		case "×":
+		case "/":
+		case "%":
 			handleOperators(buttonValue);
+			break;
+		case "=":
+			handleEval();
+			break;
+		case "C":
+			clearAll();
 			break;
 		default:
 			break;
@@ -83,65 +96,45 @@ function handleOperators(operator) {
 		return;
 	}
 
-	if (calculation.length !== 0) {
+	if (calculation.length !== 0 && !calculation.includes("=")) {
 		const calcArray = calculation.split(" ");
-		// const currentOperator = calcArray[1];
-		input = add(calcArray[0], input);
+		// Evaluate the existing operator result then append the result plus new operator
+		input = CALC_FUNCTIONS[calcArray[1]](calcArray[0], input);
 	}
 
-	console.log(operator, calculation);
 	calculation = `${input} ${operator}`;
 	operatorMode = true;
+}
+
+function handleEval() {
+	if (
+		operatorMode ||
+		!Object.keys(CALC_FUNCTIONS).some((value) => calculation.includes(value))
+	)
+		return;
+
+	calculation += ` ${input} =`;
+	const calcArray = calculation.split(" ");
+	input = CALC_FUNCTIONS[calcArray[1]](calcArray[0], input);
+	operatorMode = false;
 }
 
 function handleSignChange() {
 	input = input.charAt(0) === "-" ? input.slice(1) : (input = `-${input}`);
 }
 
+function clearAll() {
+	calculation = "";
+	input = "0";
+	operatorMode = false;
+}
+
 function evalInput() {}
 
-// Calculate methods
-function add(a, b) {
-	return (Number(a) + Number(b)).toString();
-}
-function subtract(a, b) {
-	return a - b;
-}
-function times(a, b) {
-	return a * b;
-}
-function divide(a, b) {
-	return a / b;
-}
-function module(a, b) {
-	return a % b;
-}
-function plusminus(a) {
-	return a * -1;
-}
-
-// Auxilary Functions
-
-function updateInputSpan() {
+// * UPDATE HTML INPUTS
+function updateSpan(input, span, maxLength) {
 	const len = input.length;
-
-	if (len < 8) {
-		inputSpan.textContent = input;
-		return;
-	}
-
-	// Trim input from left if longer than max-width = 8ch
-	inputSpan.textContent = `${input}`.substring(len - 8);
-}
-
-function updateCalcSpan() {
-	const len = calculation.length;
-
-	if (len < 70) {
-		calcSpan.textContent = calculation;
-		return;
-	}
-
-	// Trim calculation from left if longer than 70ch
-	inputSpan.textContent = `${calculation}`.substring(len - 70);
+	// Trim span from left if longer than max length
+	span.textContent =
+		len < maxLength ? input : `${input}`.substring(len - maxLength);
 }
